ad44e31fc6c6498e64f0520bf9746db80c4dc1a9
SKB <1215237417@qq.com>
2019-08-25 21:31:18 +0800

    add

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,22 +1,44 @@
 package com.oocl.cultivation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParkingBoy {
 
     private final ParkingLot parkingLot;
     private String lastErrorMessage;
+    private List parkingTicketsList = new ArrayList<ParkingTicket>();;
 
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+    	ParkingTicket pTicket = parkingLot.parkCar(car);
+    	if(pTicket != null) {
+    		this.lastErrorMessage = null;
+    	}else {
+    		this.lastErrorMessage = "The parking lot is full.";
+    		return null;
+    	}
+       return pTicket;
     }
 
     public Car fetch(ParkingTicket ticket) {
-        // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+    	if(ticket == null) {
+    		this.lastErrorMessage = "Please provide your parking ticket.";
+    		return null;
+    	}else if(parkingTicketsList.contains(ticket)) {
+    		this.lastErrorMessage = "Unrecognized parking ticket.";
+    		return null;
+    	}
+    	Car car = parkingLot.pickCar(ticket);
+    	if(car == null) {
+    		this.lastErrorMessage = "Unrecognized parking ticket.";
+    		return null;
+    	}
+    	parkingTicketsList.add(ticket);
+       return car;
     }
 
     public String getLastErrorMessage() {
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -18,4 +18,21 @@ public class ParkingLot {
     public int getAvailableParkingPosition() {
         return cars.size() - capacity;
     }
+    
+    public ParkingTicket parkCar(Car car) {
+    	int position = this.getAvailableParkingPosition();
+    	if(position >= 0) {
+    		return null;
+    	}
+    	ParkingTicket pTicket =new ParkingTicket();
+    	cars.put(pTicket, car);
+    	return pTicket;
+    }
+    public Car pickCar(ParkingTicket pTicket) {
+    	Car car = cars.get(pTicket);
+    	if(car == null) {
+    		return null;
+    	}
+    	return car;
+    }
 }
